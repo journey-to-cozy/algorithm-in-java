1fd1717d7284ce96b0c5c6e12f0e9fbba47efac2
sana-20 <oshannon.dev@gmail.com>
2021-08-05 08:50:38 +0900

    ✨ 6장 문자열 조작 풀이

---
+++ 344_reverse_string.java
@@ -0,0 +1,18 @@
+package src;
+
+class ReverseString {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left++] = s[right];
+            s[right--] = tmp;
+
+//                char temp = s[left];
+//                s[left] = s[right];
+//                s[right] = temp;
+//                left += 1;
+//                right -= 1;
+        }
+    }
+}
