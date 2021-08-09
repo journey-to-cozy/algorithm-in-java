0e90e7182ec7b3d7b82d109947dd6c4f09d7d171
geniushyeon <jhlee0912@icloud.com>
2021-08-09 23:34:23 +0900

    ♻️ TwoSum: flag 변수 제거

--- src/com/algorithm/array/TwoSum.java
+++ src/com/algorithm/array/TwoSum.java
@@ -7,19 +7,15 @@ public class TwoSum {
 	// brute force - O(n^2), 52ms
 	public int[] twoSum(int[] nums, int target) {
 		int[] result = new int[2];
-		boolean flag = false;
 
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = i + 1; j < nums.length; j++) {
 				if (nums[i] + nums[j] == target) {
 					result[0] = i;
 					result[1] = j;
-					flag = true;
-					break;
+					return result;
 				}
 			}
-			if (flag)
-				break;
 		}
 		return result;
 	}
