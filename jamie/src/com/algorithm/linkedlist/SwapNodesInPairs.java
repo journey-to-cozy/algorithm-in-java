5404a9c262405ceced60a5b02abe482883806647
geniushyeon <jhlee0912@icloud.com>
2021-08-16 23:48:29 +0900

    ✨ 노드 스왑 - 코코 코드 추가 및 공부

--- jamie/src/com/algorithm/linkedlist/SwapNodesInPairs.java
+++ jamie/src/com/algorithm/linkedlist/SwapNodesInPairs.java
@@ -30,4 +30,24 @@ public class SwapNodesInPairs {
 
 		return n2;
 	}
+
+	// iterative
+	public ListNode swapPairsByCoco(ListNode head) {
+		ListNode temp = new ListNode(0);
+		temp.next = head;
+		ListNode current = temp;
+
+		while (current.next != null && current.next.next != null) {
+			ListNode first = current.next;
+			ListNode second = current.next.next;
+
+			first.next = second.next;
+			current.next = second;
+			current.next.next = first;
+
+			current = current.next.next;
+		}
+
+		return temp.next;
+	}
 }
