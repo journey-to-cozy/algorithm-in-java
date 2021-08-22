2e28d1f35b47b475f454c37b839fda2c583098ba
sana-20 <oshannon.dev@gmail.com>
2021-08-22 13:13:03 +0900

    🔊 739번 문제 결과값 로그 추가

--- sanga/739_daily_temperatures.java
+++ sanga/739_daily_temperatures.java
@@ -11,7 +11,7 @@ class DailyTemperatures {
     public static void main(String[] args) {
         DailyTemperatures dailyTemperatures = new DailyTemperatures();
         int[] input = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
-        System.out.println(Arrays.toString(dailyTemperatures.dailyTemperatures(input)));
+        System.out.println(Arrays.toString(dailyTemperatures.dailyTemperatures(input))); // [1, 1, 4, 2, 1, 1, 0, 0]
     }
 
     public int[] dailyTemperatures(int[] temperatures) {
