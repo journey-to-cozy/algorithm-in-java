a25f8a7622d433378f6efe73af76fc716414d680
sana-20 <oshannon.dev@gmail.com>
2021-09-03 07:35:21 +0900

    ✨ 데크 이중 연결 리스트로 구현한 코드 추가

--- sanga/641_design_circular_deque.java
+++ sanga/641_design_circular_deque.java
@@ -84,3 +84,102 @@ class MyCircularDeque {
     }
 }
 
+
+
+/**
+ * 원형 데크 디자인 - 이중 연결 리스트 방식으로 풀어보기
+ */
+class DesignCircularDequeUsingDoubleLinkedList {
+
+    class MyCircularDeque {
+        int size;
+        int k;
+        DoubleListNode head;
+        DoubleListNode tail;
+
+        public MyCircularDeque(int k) {
+            head = new DoubleListNode(-1);
+            tail = new DoubleListNode(-1);
+            head.pre = tail;
+            tail.next = head;
+            this.k = k;
+            this.size = 0;
+        }
+
+        public boolean insertFront(int value) {
+            if (isFull()) {
+                return false;
+            }
+            DoubleListNode node = new DoubleListNode(value);
+            node.next = head;
+            node.pre = head.pre;
+            head.pre.next = node;
+            head.pre = node;
+            size++;
+
+            return true;
+        }
+
+        public boolean insertLast(int value) {
+            if (isFull()) {
+                return false;
+            }
+            DoubleListNode node = new DoubleListNode(value);
+            node.next = tail.next;
+            tail.next.pre = node;
+            tail.next = node;
+            node.pre = tail;
+            size++;
+
+            return true;
+        }
+
+        public boolean deleteFront() {
+            if (isEmpty()) {
+                return false;
+            }
+            head.pre.pre.next = head;
+            head.pre = head.pre.pre;
+            size--;
+
+            return true;
+        }
+
+        public boolean deleteLast() {
+            if (isEmpty()) {
+                return false;
+            }
+            tail.next.next.pre = tail;
+            tail.next = tail.next.next;
+            size--;
+
+            return true;
+        }
+
+        public int getFront() {
+            return head.pre.val;
+        }
+
+        public int getRear() {
+            return tail.next.val;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public boolean isFull() {
+            return size == k;
+        }
+    }
+
+    class DoubleListNode {
+        DoubleListNode pre;
+        DoubleListNode next;
+        int val;
+
+        public DoubleListNode(int val) {
+            this.val = val;
+        }
+    }
+}
