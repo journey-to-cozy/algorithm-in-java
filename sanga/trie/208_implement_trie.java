e08eae038eed2675504ab1e6fd5627b8b52b3952
sana-20 <oshannon.dev@gmail.com>
2022-01-22 15:46:55 +0900

    ⚡ 208번 문제 풀이 추가

--- sanga/trie/208_implement_trie.java
+++ sanga/trie/208_implement_trie.java
@@ -1,7 +1,5 @@
 package src.sanga.trie;
 
-import sun.tools.jconsole.Worker;
-
 /**
  * https://leetcode.com/problems/implement-trie-prefix-tree/
  */
@@ -24,41 +22,71 @@ class Trie {
     }
 
     public void insert(String word) {
-        TrieNode ws = root;
+        TrieNode node = root;
         for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (ws.children[c - 'a'] == null) {
-                ws.children[c - 'a'] = new TrieNode();
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
             }
-            ws = ws.children[c - 'a'];
+            node = node.get(currentChar);
         }
-        ws.isWord = true;
+        node.setEnd();
+    }
+
+    public TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char curLetter = word.charAt(i);
+            if (node.containsKey(curLetter)) {
+                node = node.get(curLetter);
+            } else {
+                return null;
+            }
+        }
+        return node;
     }
 
     public boolean search(String word) {
-        TrieNode ws = root;
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
-        }
-        return ws.isWord;
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
     }
 
     public boolean startsWith(String prefix) {
-        TrieNode ws = root;
-        for (int i = 0; i < prefix.length(); i++) {
-            char c = prefix.charAt(i);
-            if (ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
-        }
-        return true;
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
     }
+
 }
 
 class TrieNode {
-    public boolean isWord;
-    public TrieNode[] children = new TrieNode[26];
 
-    public TrieNode() { }
+    private TrieNode[] links;
+
+    private final int R = 26;
+
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
+    }
+
+    public TrieNode get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    public void put(char ch, TrieNode node) {
+        links[ch - 'a'] = node;
+    }
+
+    public void setEnd() {
+        isEnd = true;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
 }
